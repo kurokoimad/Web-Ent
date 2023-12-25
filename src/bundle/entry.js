563f081d37bebee1cae74a2108787cb2c159addb
kurokoimad <widadimad2000@gmail.com>
2023-12-25 12:14:08 +0100

    small modif

--- src/bundle/entry.js
+++ src/bundle/entry.js
@@ -1,5 +1,6 @@
 import '../app.js';
 import '../lib/server/db.js';
+import '../lib/server/db.js';
 import '../lib/server/debug.js';
 import '../lib/server/profile.js';
 import '../lib/server/profileController.js';
