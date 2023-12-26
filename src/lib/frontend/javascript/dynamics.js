a2cc4490d648649fe18ede3f22ed9ce484591a3e
kurokoimad <widadimad2000@gmail.com>
2023-12-26 13:44:10 +0100

    smoll change2

--- src/lib/frontend/javascript/dynamics.js
+++ src/lib/frontend/javascript/dynamics.js
@@ -1,17 +1,17 @@
-// IT SHOULD NOT BE FUNCTIONAL YET !!!
+// // IT SHOULD NOT BE FUNCTIONAL YET !!!
 
-function changePage(page) {
-  // Include the current gender filter in the URL
-  window.location.href = `?page=${page}&gender=${currentGenderFilter || ""}`;
-}
+// function changePage(page) {
+//   // Include the current gender filter in the URL
+//   window.location.href = `?page=${page}&gender=${currentGenderFilter || ""}`;
+// }
 
-function filterByGender(gender) {
-  // Update the currentGenderFilter variable
-  currentGenderFilter = gender;
-  // Redirect to the first page with the selected gender filter
-  window.location.href = `?page=1&gender=${currentGenderFilter || ""}`;
-}
+// function filterByGender(gender) {
+//   // Update the currentGenderFilter variable
+//   currentGenderFilter = gender;
+//   // Redirect to the first page with the selected gender filter
+//   window.location.href = `?page=1&gender=${currentGenderFilter || ""}`;
+// }
 
-// Initialize the currentGenderFilter variable only if it's not null or undefined
-let currentGenderFilter =
-  '<%= currentGenderFilter !== null && typeof currentGenderFilter !== "undefined" ? currentGenderFilter : "" %>';
+// // Initialize the currentGenderFilter variable only if it's not null or undefined
+// let currentGenderFilter =
+//   '<%= currentGenderFilter !== null && typeof currentGenderFilter !== "undefined" ? currentGenderFilter : "" %>';
